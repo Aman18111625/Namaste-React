2c080357a7d4e387974bfb750703bbdc75acdbbf
Aman18111625 <amangupta326519@gmail.com>
2023-09-06 00:09:58 +0530

    day-2

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello world!");
-console.log(heading);//object
-const root = ReactDOM.createRoot(document.getElementById("root-container"));
-root.render(heading);
-//this render function accept this object and convert into a heading tag before that it's only a normal js object not a html element
+const parent = React.createElement("div", {id: "parent"},[
+    React.createElement("div",{id: "child"}, [
+        React.createElement("h1", {}, "I'm a h1 tag"),
+        React.createElement("h2", {}, "I'm a h2 tag")
+    ]),
+    React.createElement("div", {id: "child2"},[
+        React.createElement("h1", {}, "I'm a h1 tag"),
+        React.createElement("h2", {}, "I'm a h2 tag")
+    ])
+])
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);//render function always replace whatever inside our root , not append
