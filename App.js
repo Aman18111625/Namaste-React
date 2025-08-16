44383da9f99ddb53352566dbfa5c82452fdb60ef
Aman1811625 <amangupta326519@gmail.com>
2025-08-16 20:33:12 +0530

    Moved Sample Data into Array & Then using it with map

--- App.js
+++ App.js
@@ -34,6 +34,81 @@ const RestaurantCard = (props) => {
     )
 }
 
+const restaurantData = [
+  {
+    id: 'id_1',
+    resName: "Meghna Foods",
+    cuisine: "Biryani, North Indian, Asian",
+    ratings: "4.5",
+    deliveryTime: "30 mins",
+    costForTwo: "₹600"
+  },
+  {
+    id: 'id_2',
+    resName: "KFC",
+    cuisine: "Burgers, Fast Food",
+    ratings: "4.2",
+    deliveryTime: "25 mins",
+    costForTwo: "₹800"
+  },
+  {
+    id: 'id_3',
+    resName: "Rameshwaram Cafe",
+    cuisine: "South Indian, North Indian",
+    ratings: "4.1",
+    deliveryTime: "35 mins",
+    costForTwo: "₹500"
+  },
+  {
+    id: 'id_4',
+    resName: "Domino's Pizza",
+    cuisine: "Italian, Pizza",
+    ratings: "4.7",
+    deliveryTime: "30 mins",
+    costForTwo: "₹700"
+  },
+  {
+    id: 'id_5',
+    resName: "Asha Tiffin",
+    cuisine: "North Indian, South Indian",
+    ratings: "4.0",
+    deliveryTime: "25 mins",
+    costForTwo: "₹400"
+  },
+  {
+    id: 'id_6',
+    resName: "Oven Story",
+    cuisine: "Italian, Pizza",
+    ratings: "4.0",
+    deliveryTime: "25 mins",
+    costForTwo: "₹600"
+  },
+  {
+    id: 'id_7',
+    resName: "Taco Bell",
+    cuisine: "Mexican, Tacos",
+    ratings: "4.3",
+    deliveryTime: "40 mins",
+    costForTwo: "₹500"
+  },
+  {
+    id: 'id_8',
+    resName: "Chinese Food Point",
+    cuisine: "Chinese, Noodles",
+    ratings: "3.8",
+    deliveryTime: "25 mins",
+    costForTwo: "₹300"
+  },
+  {
+    id: 'id_9',
+    resName: "Sangam Sweets",
+    cuisine: "Sweets, Gujarati",
+    ratings: "4.8",
+    deliveryTime: "25 mins",
+    costForTwo: "₹200"
+  }
+]
+
 const Body = () => {
     return (
         <div className="body-container">
@@ -42,60 +117,15 @@ const Body = () => {
               <div className='search-button'> Search </div>
             </div>
             <div className="restaurant-container">
-                <RestaurantCard 
-                  resName="Meghna Foods"
-                  cuisine="Biryani, North Indian, Asian"
-                  ratings="4.5"
-                  deliveryTime="30 mins"
-                />
-                <RestaurantCard 
-                  resName="KFC"
-                  cuisine="Burgers, Fast Food"
-                  ratings="4.2"
-                  deliveryTime="25 mins"
-                />
-                <RestaurantCard 
-                  resName="Rameshwaram Cafe"
-                  cuisine="South Indian, North Indian"
-                  ratings="4.1"
-                  deliveryTime="35 mins"
-                />
-                <RestaurantCard 
-                  resName="Domino's Pizza"
-                  cuisine="Italian, Pizza"
-                  ratings="4.7"
-                  deliveryTime="30 mins"
-                />
-                <RestaurantCard 
-                  resName="Asha Tiffin"
-                  cuisine="North Indian, South Indian"
-                  ratings="4.0"
-                  deliveryTime="25 mins"
-                />
-                <RestaurantCard 
-                  resName="Oven Story"
-                  cuisine="Italian, Pizza"
-                  ratings="4.0"
-                  deliveryTime="25 mins"
-                />
-                <RestaurantCard 
-                  resName="Taco Bell"
-                  cuisine="Mexican, Tacos"
-                  ratings="4.3"
-                  deliveryTime="40 mins"
-                />
-                <RestaurantCard 
-                  resName="Chinese Food Point"
-                  cuisine="Chinese, Noodles"
-                  ratings="3.8"
-                  deliveryTime="25 mins"
-                />
-                <RestaurantCard 
-                  resName="Sangam Sweets"
-                  cuisine="Sweets, Gujarati"
-                  ratings="4.8"
-                  deliveryTime="25 mins"
-                />
+                {restaurantData.map((restaurant) => (
+                    <RestaurantCard 
+                        key={restaurant.id}
+                        resName={restaurant.resName}
+                        cuisine={restaurant.cuisine}
+                        ratings={restaurant.ratings}
+                        deliveryTime={restaurant.deliveryTime}
+                    />
+                ))}
             </div>
         </div>
     )
