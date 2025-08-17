3c6df377c6dfb7c1f70b3de37bf717b9282919da
Aman1811625 <amangupta326519@gmail.com>
2025-08-18 01:08:21 +0530

    Restrucutring of code

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import { LOGO_URL } from "../utils.js/constant";
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <div className="logo-container">
+              <img src={LOGO_URL} alt="Logo" className="logo" />
+            </div>
+            <div className="nav-container">
+              <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+              </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
