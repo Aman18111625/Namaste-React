f90c632a6dc8f455f3a193ff54bd29cf2ae360d2
Aman1811625 <amangupta326519@gmail.com>
2025-08-19 00:41:10 +0530

    Day-1

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,35 +1,57 @@
 import RestaurantCard from "./RestaurentCard";
-import { restaurantData} from "../utils.js/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [restData, setRestData] = useState(restaurantData);
+    const [restData, setRestData] = useState([]);
 
     const onClickFilter = () => {
-       const filteredRes = restData.filter((restaurant) => restaurant.ratings > "4.5");
+       const filteredRes = restData.filter((restaurant) => restaurant?.info?.avgRating > 4.3);
        setRestData(filteredRes);
     }
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const swiggyAPI = fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const response = await swiggyAPI;
+        const data = await response.json();
+        const restaurants = data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+        setRestData(restaurants);
+    }
+
+    if (restData?.length === 0) {
+        return <Shimmer/>
+    }
+
     return (
-        <div className="body-container">
-            {/* <div className='search-container'>
-              <input type="text" placeholder="Search for restaurants..." className='search-input' />
-              <button className='search-button'> Search </button>
-            </div> */}
+        restData?.length === 0 ? (
+            <Shimmer />
+        ) : (
+            <div className="body-container">
+                {/* <div className='search-container'>
+                  <input type="text" placeholder="Search for restaurants..." className='search-input' />
+                  <button className='search-button'> Search </button>
+                </div> */}
             <div className='filter-container'>
-              <button className='filter-button' onClick={() => onClickFilter()}> Top Rated Restaurant </button>
+              <button className='filter-button' onClick={() => onClickFilter()}> <strong>Top Rated Restaurant</strong> </button>
             </div>
             <div className="restaurant-container">
                 {restData.map((restaurant) => (
                     <RestaurantCard 
-                        key={restaurant.id}
-                        resName={restaurant.resName}
-                        cuisine={restaurant.cuisine}
-                        ratings={restaurant.ratings}
-                        deliveryTime={restaurant.deliveryTime}
+                        key={restaurant?.info?.id}
+                        resName={restaurant?.info?.name}
+                        cuisine={restaurant?.info?.cuisines.join(", ")}
+                        ratings={restaurant?.info?.avgRating}
+                        deliveryTime={restaurant?.info?.sla?.deliveryTime}
+                        cloudinaryImageId={restaurant?.info?.cloudinaryImageId}
                     />
                 ))}
             </div>
         </div>
+        )
     )
 }
 
