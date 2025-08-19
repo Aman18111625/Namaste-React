43c246cbb226e5eb34d1506ffa6676a5284224d5
Aman1811625 <amangupta326519@gmail.com>
2025-08-20 00:03:47 +0530

    episode-6 Day-2

--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -1,17 +1,21 @@
 const RestaurantCard = (props) => {
-    // Destructuring props
-    const { resName, cuisine, ratings, deliveryTime, cloudinaryImageId } = props;
-    return (
-        <div className="restaurant-card">
-            <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${cloudinaryImageId}`} alt="Restaurant" className='restaurant-image' />
-            <div className="restaurant-info">
-                <h5>{resName}</h5>
-                <h5>{cuisine}</h5>
-                <h5>{ratings}</h5>
-                <h5>{deliveryTime}</h5>
-            </div>
-        </div>
-    )
-}
+  // Destructuring props
+  const { resName, cuisine, ratings, deliveryTime, cloudinaryImageId } = props;
+  return (
+    <div className="restaurant-card">
+      <img
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${cloudinaryImageId}`}
+        alt="Restaurant"
+        className="restaurant-image"
+      />
+      <div className="restaurant-info">
+        <h5>{resName}</h5>
+        <h5>{cuisine}</h5>
+        <h5>{ratings}</h5>
+        <h5>{deliveryTime}</h5>
+      </div>
+    </div>
+  );
+};
 
 export default RestaurantCard;
