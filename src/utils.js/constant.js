10f7f1b0a9cedbc955cc5d0c7aa533d1b5da9e8a
Aman1811625 <amangupta326519@gmail.com>
2025-08-23 20:06:11 +0530

    Cors Issue Resolved

--- src/utils.js/constant.js
+++ src/utils.js/constant.js
@@ -1,3 +1,5 @@
 export const LOGO_URL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuTrVbvtGgpES5LypFyiffQR9oNUrVHcnDkA&s';
 
 export const NO_RESULT_IMAGE_URL = 'https://cdn.dribbble.com/userupload/2905353/file/original-2022966da1fc3718d3feddfdc471ae47.png?resize=400x0';
+
+export const CLOUDINARY_IMAGE_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/';
